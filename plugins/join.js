46a0c45c0c478ebe0009217a4e8bd9ce45fb8b3e
nanisabrie <94164611+nanisabrie@users.noreply.github.com>
2022-07-09 11:42:51 +0800

    Update join.js
    Ddd

--- plugins/join.js
+++ plugins/join.js
@@ -10,8 +10,7 @@ let imgr = thumbnailUrl.getRandom()
     let res = await conn.groupAcceptInvite(code)
     if (!res) throw res.toString()
     let name = await conn.getName(res).catch(_ => null)
-    expired = Math.floor(Math.min(1, Math.max(999, isOwner ? expired && expired.isNumber() ? parseInt(expired) : 0 : 3)))
-    let caption = `*Berhasil join grup* ${name || res} ${expired ? `selama *${expired}* hari` : ''}\n*Jangan lupa baca rules ngap!*`
+    expired = Math.floor(Math.min(5, Math.max(999, isOwner ? expired && expired.isNumber() ? parseInt(expired) : 0 : 3)))    let caption = `*Berhasil join grup* ${name || res} ${expired ? `selama *${expired}* hari` : ''}\n*Jangan lupa baca rules ngap!*`
     await conn.sendButton(m.chat, caption, wm, imgr, [
                 ['Rules', `${usedPrefix}rules`]
             ], m, frep)
