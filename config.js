50b11766c0e517a6b0274563e2af18f9148a97fd
nanisabrie <94164611+nanisabrie@users.noreply.github.com>
2022-07-09 10:02:00 +0800

    Update config.js

--- config.js
+++ config.js
@@ -372,7 +372,7 @@ global.wait = 'Wait...'
 global.render = 'Render...'
 global.webs = 'https://s.id/Cerdasin62/'//Your Website
 global.logo = "https://i.pinimg.com/564x/f7/d2/e4/f7d2e48fd59a8c01cd396bfc70b0a2d1.jpg"//Your logo
-global.gcwangsaf = "https://chat.whatsapp.com/I2cagI7uWN59oxMomPQvfM"//Link Group whatsapp
+global.gcwangsaf = "https://chat.whatsapp.com/DtyPenO5Rha7yTWqr5gR8J"//Link Group whatsapp
 
 /*============== PAYMENT ==============*/
 global.tng = ["1161739141"]
