32ba39c0bac86695d95ae29ae0b2afdbdb8475eb
nanisabrie <94164611+nanisabrie@users.noreply.github.com>
2022-07-09 09:07:35 +0800

    Update menu.js
    Kk

--- plugins/menu.js
+++ plugins/menu.js
@@ -187,7 +187,7 @@ let handler = async (m, { conn, groupMetadata, usedPrefix: _p, __dirname }) => {
     text = text.replace(new RegExp(`%(${Object.keys(replace).sort((a, b) => b.length - a.length).join`|`})`, 'g'), (_, name) => '' + replace[name])
     //
     
-    conn.sendHydrated(m.chat, text.trim(), wm + '\n\n' + botdate, hwaifu.getRandom(), gcwangsaf, 'Hinata Group', who.split`@`[0], 'Your Number', [
+    conn.sendHydrated(m.chat, text.trim(), wm + '\n\n' + botdate, hwaifu.getRandom(), gcwangsaf, 'Group', who.split`@`[0], 'Your Number', [
       ['Menu', '/menu'],
       ['Owner', '/owner'],
       ['Test', '/ping']
